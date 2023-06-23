503d15425e48b0a9e109c3d9ef672f47c40fece4
Adarsh Mishra <adjee2020@gmail.com>
2023-06-24 05:19:18 +0530

    completed

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login } from "../controllers/auth.js";
-
-// router is an objest
-const router = express.Router();
-
-router.post("/login", login);
-
-export default router;
